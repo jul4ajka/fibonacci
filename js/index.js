705298d620033c580664fd76fa82900dc4fe787c
JuliaChayka <juliachayka133@yahoo.com>
2017-02-27 14:49:51 +0000

    New

--- js/index.js
+++ js/index.js
@@ -9,16 +9,16 @@
     document.write(numbersFibonacci[n-1]);
 
 // recursive method
-// var n = Number(prompt('Please enter the sequence number of Fibonacci numbers'));
-// function fib(n){
-//     if (n === 1) {
-//         return 1;
-//     } 
-//     else if (n === 2){
-//         return  1;
-//     }
-//     else {
-//      return fib(n-1) + fib(n-2);
-//   }
-// }
-// document.write(fib(n));
+var n = Number(prompt('Please enter the sequence number of Fibonacci numbers'));
+function fib(n){
+    if (n === 1) {
+        return 1;
+    } 
+    else if (n === 2){
+        return  1;
+    }
+    else {
+     return fib(n-1) + fib(n-2);
+  }
+}
+document.write(fib(n));
